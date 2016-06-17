a6a631e573ea1f8f37d3014528641f0e3d8e7010
Alexey Yegorov <yegorov.alex@gmail.com>
2016-06-17 20:36:02 +0300

    [elements][build] fixed path (Windows)

--- elements/Android.mk
+++ elements/Android.mk
@@ -1,15 +1,29 @@
 LOCAL_PATH := $(call my-dir)
-PROJECT_ROOT:= $(LOCAL_PATH)/..
 
 # libpng
 # ------------------------------------
 include $(CLEAR_VARS)
 
-LIBPNG_PATH := $(PROJECT_ROOT)/third-party/libpng
+LIBPNG_PATH := ../third-party/libpng
 LOCAL_MODULE := png
 LOCAL_CFLAGS := -O3 -s -DNDEBUG -ffast-math
-LOCAL_SRC_FILES := $(wildcard $(LIBPNG_PATH)/*.c)
-LOCAL_EXPORT_C_INCLUDES := $(LIBPNG_PATH)
+LOCAL_SRC_FILES := $(LIBPNG_PATH)/png.c \
+	$(LIBPNG_PATH)/pngerror.c \
+	$(LIBPNG_PATH)/pngget.c \
+	$(LIBPNG_PATH)/pngmem.c \
+	$(LIBPNG_PATH)/pngpread.c \
+	$(LIBPNG_PATH)/pngread.c \
+	$(LIBPNG_PATH)/pngrio.c \
+	$(LIBPNG_PATH)/pngrtran.c \
+	$(LIBPNG_PATH)/pngrutil.c \
+	$(LIBPNG_PATH)/pngset.c \
+	$(LIBPNG_PATH)/pngtest.c \
+	$(LIBPNG_PATH)/pngtrans.c \
+	$(LIBPNG_PATH)/pngwio.c \
+	$(LIBPNG_PATH)/pngwrite.c \
+	$(LIBPNG_PATH)/pngwtran.c \
+	$(LIBPNG_PATH)/pngwutil.c
+LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/$(LIBPNG_PATH)
 LOCAL_EXPORT_LDLIBS := -lz
 include $(BUILD_STATIC_LIBRARY)
 
@@ -17,18 +31,18 @@ include $(BUILD_STATIC_LIBRARY)
 # ------------------------------------
 include $(CLEAR_VARS)
 
-PUGIXML_PATH := $(PROJECT_ROOT)/third-party/pugixml
+PUGIXML_PATH := ../third-party/pugixml
 LOCAL_MODULE := pugixml
 LOCAL_CPPFLAGS  := -std=c++14 -O3 -s -DNDEBUG -ffast-math -DPUGIXML_NO_EXCEPTIONS
 LOCAL_SRC_FILES := $(PUGIXML_PATH)/src/pugixml.cpp
-LOCAL_EXPORT_C_INCLUDES := $(PUGIXML_PATH)/src
+LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/$(PUGIXML_PATH)/src
 include $(BUILD_STATIC_LIBRARY)
 
 #freetype
 #-------------------------------------
 include $(CLEAR_VARS)
 
-FREETYPE_PATH := $(PROJECT_ROOT)/third-party/freetype-2.6.2
+FREETYPE_PATH := ../third-party/freetype-2.6.2
 LOCAL_MODULE := freetype
 LOCAL_CFLAGS := -O3 -s -DNDEBUG -ffast-math -DFT2_BUILD_LIBRARY
 LOCAL_SRC_FILES := $(FREETYPE_PATH)/src/autofit/autofit.c \
@@ -72,8 +86,8 @@ LOCAL_SRC_FILES := $(FREETYPE_PATH)/src/autofit/autofit.c \
     $(FREETYPE_PATH)/src/type1/type1.c \
     $(FREETYPE_PATH)/src/type42/type42.c \
     $(FREETYPE_PATH)/src/winfonts/winfnt.c
-LOCAL_C_INCLUDES := $(FREETYPE_PATH)/include \
-    $(FREETYPE_PATH)/src
+LOCAL_C_INCLUDES := $(LOCAL_PATH)/$(FREETYPE_PATH)/include \
+    $(LOCAL_PATH)/$(FREETYPE_PATH)/src
 LOCAL_EXPORT_C_INCLUDES := $(LOCAL_C_INCLUDES)
 include $(BUILD_STATIC_LIBRARY)
 
@@ -81,53 +95,52 @@ include $(BUILD_STATIC_LIBRARY)
 # ------------------------------------
 include $(CLEAR_VARS)
 
-ELEMENTS_PATH := $(PROJECT_ROOT)/elements
 LOCAL_MODULE := elements_engine
 LOCAL_CPPFLAGS  := -std=c++14 -O3 -s -DNDEBUG -ffast-math -Wall -Wextra -Werror
-LOCAL_SRC_FILES := $(ELEMENTS_PATH)/rendering/core/shader.cpp \
-    $(ELEMENTS_PATH)/rendering/core/program.cpp \
-    $(ELEMENTS_PATH)/rendering/computation/compute_product_index.cpp \
-    $(ELEMENTS_PATH)/rendering/computation/compute_target.cpp \
-    $(ELEMENTS_PATH)/rendering/primitives/square.cpp \
-    $(ELEMENTS_PATH)/rendering/effects/light_scattered.cpp \
-    $(ELEMENTS_PATH)/rendering/effects/blur.cpp \
-    $(ELEMENTS_PATH)/rendering/effects/gradient.cpp \
-    $(ELEMENTS_PATH)/rendering/effects/tone.cpp \
-    $(ELEMENTS_PATH)/rendering/effects/blend.cpp \
-    $(ELEMENTS_PATH)/rendering/effects/clear.cpp \
-    $(ELEMENTS_PATH)/rendering/passes/pass_target_placement.cpp \
-    $(ELEMENTS_PATH)/rendering/passes/pass_target.cpp \
-    $(ELEMENTS_PATH)/rendering/passes/pass_target_input.cpp \
-    $(ELEMENTS_PATH)/rendering/passes/pass_target_storage.cpp \
-    $(ELEMENTS_PATH)/rendering/passes/pass_composition.cpp \
-    $(ELEMENTS_PATH)/rendering/utils/program_loader.cpp \
-    $(ELEMENTS_PATH)/ui/freetype/manager.cpp \
-    $(ELEMENTS_PATH)/ui/freetype/line.cpp \
-    $(ELEMENTS_PATH)/ui/system.cpp \
-    $(ELEMENTS_PATH)/ui/control.cpp \
-    $(ELEMENTS_PATH)/ui/controls/button.cpp \
-    $(ELEMENTS_PATH)/ui/controls/panel.cpp \
-    $(ELEMENTS_PATH)/ui/controls/label.cpp \
-    $(ELEMENTS_PATH)/ui/controls/slider.cpp \
-    $(ELEMENTS_PATH)/assets/assets_storage.cpp \
-    $(ELEMENTS_PATH)/assets/asset_texture.cpp \
-    $(ELEMENTS_PATH)/assets/asset_blob.cpp \
-    $(ELEMENTS_PATH)/assets/asset_xml.cpp \
-    $(ELEMENTS_PATH)/assets/asset_operations_stream.cpp \
-    $(ELEMENTS_PATH)/preferences/preferences.cpp \
-    $(ELEMENTS_PATH)/metrics/metrics.cpp \
-    $(ELEMENTS_PATH)/timing/framerate.cpp \
-    $(ELEMENTS_PATH)/timing/update.cpp \
-    $(ELEMENTS_PATH)/timing/timer.cpp \
-    $(ELEMENTS_PATH)/simulation/liquid/system.cpp \
-    $(ELEMENTS_PATH)/simulation/liquid/volume.cpp \
-    $(ELEMENTS_PATH)/simulation/liquid/config.cpp \
-    $(ELEMENTS_PATH)/simulation/air/system.cpp \
-    $(ELEMENTS_PATH)/simulation/air/volume.cpp
+LOCAL_SRC_FILES := rendering/core/shader.cpp \
+    rendering/core/program.cpp \
+    rendering/computation/compute_product_index.cpp \
+    rendering/computation/compute_target.cpp \
+    rendering/primitives/square.cpp \
+    rendering/effects/light_scattered.cpp \
+    rendering/effects/blur.cpp \
+    rendering/effects/gradient.cpp \
+    rendering/effects/tone.cpp \
+    rendering/effects/blend.cpp \
+    rendering/effects/clear.cpp \
+    rendering/passes/pass_target_placement.cpp \
+    rendering/passes/pass_target.cpp \
+    rendering/passes/pass_target_input.cpp \
+    rendering/passes/pass_target_storage.cpp \
+    rendering/passes/pass_composition.cpp \
+    rendering/utils/program_loader.cpp \
+    ui/freetype/manager.cpp \
+    ui/freetype/line.cpp \
+    ui/system.cpp \
+    ui/control.cpp \
+    ui/controls/button.cpp \
+    ui/controls/panel.cpp \
+    ui/controls/label.cpp \
+    ui/controls/slider.cpp \
+    assets/assets_storage.cpp \
+    assets/asset_texture.cpp \
+    assets/asset_blob.cpp \
+    assets/asset_xml.cpp \
+    assets/asset_operations_stream.cpp \
+    preferences/preferences.cpp \
+    metrics/metrics.cpp \
+    timing/framerate.cpp \
+    timing/update.cpp \
+    timing/timer.cpp \
+    simulation/liquid/system.cpp \
+    simulation/liquid/volume.cpp \
+    simulation/liquid/config.cpp \
+    simulation/air/system.cpp \
+    simulation/air/volume.cpp
 
-LOCAL_C_INCLUDES := $(PROJECT_ROOT) \
-    $(PROJECT_ROOT)/elements \
-    $(PROJECT_ROOT)/third-party/glm
+LOCAL_C_INCLUDES := $(LOCAL_PATH)/.. \
+    $(LOCAL_PATH) \
+    $(LOCAL_PATH)/../third-party/glm
 LOCAL_EXPORT_C_INCLUDES := $(LOCAL_C_INCLUDES)
 LOCAL_EXPORT_LDLIBS := -latomic -lGLESv2 -llog -L$(SYSROOT)/usr/lib
 LOCAL_STATIC_LIBRARIES := png freetype pugixml
--- experiments/Android.mk
+++ experiments/Android.mk
@@ -1,57 +1,55 @@
 LOCAL_PATH := $(call my-dir)
-PROJECT_ROOT:= $(LOCAL_PATH)/..
 
 # experiments
 # ------------------------------------
 
 include $(CLEAR_VARS)
 
-EXPERIMENTS_PATH := $(PROJECT_ROOT)/experiments
 LOCAL_MODULE := experiments
 LOCAL_CPPFLAGS  := -std=c++14 -O3 -s -DNDEBUG -ffast-math -Wall -Wextra -Werror
 
 ifeq ($(BUILD_FIRE), true)
-	LOCAL_SRC_FILES += $(EXPERIMENTS_PATH)/fire/renderer.cpp \
-		$(EXPERIMENTS_PATH)/fire/pass_raymarching.cpp \
-		$(EXPERIMENTS_PATH)/fire/pass_fire.cpp
+	LOCAL_SRC_FILES += fire/renderer.cpp \
+		fire/pass_raymarching.cpp \
+		fire/pass_fire.cpp
 endif
 
 ifeq ($(BUILD_LIGHT), true)
-	LOCAL_SRC_FILES += $(EXPERIMENTS_PATH)/light/pass_particles.cpp \
-		$(EXPERIMENTS_PATH)/light/pass_occluding.cpp \
-		$(EXPERIMENTS_PATH)/light/pass_velocities.cpp \
-		$(EXPERIMENTS_PATH)/light/pass_positions.cpp \
-		$(EXPERIMENTS_PATH)/light/renderer.cpp
+	LOCAL_SRC_FILES += light/pass_particles.cpp \
+		light/pass_occluding.cpp \
+		light/pass_velocities.cpp \
+		light/pass_positions.cpp \
+		light/renderer.cpp
 endif
 
 ifeq ($(BUILD_STRANGE), true)
-	LOCAL_SRC_FILES += $(EXPERIMENTS_PATH)/strange/attractors.cpp \
-		$(EXPERIMENTS_PATH)/strange/renderer.cpp \
-		$(EXPERIMENTS_PATH)/strange/particles_product.cpp \
-		$(EXPERIMENTS_PATH)/strange/pass_positions.cpp \
-		$(EXPERIMENTS_PATH)/strange/pass_particles.cpp
+	LOCAL_SRC_FILES += strange/attractors.cpp \
+		strange/renderer.cpp \
+		strange/particles_product.cpp \
+		strange/pass_positions.cpp \
+		strange/pass_particles.cpp
 endif
 
 ifeq ($(BUILD_AIR), true)
-	LOCAL_SRC_FILES += $(EXPERIMENTS_PATH)/air/pass_velocities.cpp \
-		$(EXPERIMENTS_PATH)/air/pass_positions.cpp \
-		$(EXPERIMENTS_PATH)/air/pass_particles.cpp \
-		$(EXPERIMENTS_PATH)/air/renderer.cpp
+	LOCAL_SRC_FILES += air/pass_velocities.cpp \
+		air/pass_positions.cpp \
+		air/pass_particles.cpp \
+		air/renderer.cpp
 endif
 
 ifeq ($(BUILD_LIQUID), true)
-	LOCAL_SRC_FILES += $(EXPERIMENTS_PATH)/liquid/renderer.cpp \
-		$(EXPERIMENTS_PATH)/liquid/pass_particles.cpp \
-		$(EXPERIMENTS_PATH)/liquid/pass_metaballs.cpp \
-		$(EXPERIMENTS_PATH)/liquid/pass_liquid.cpp
+	LOCAL_SRC_FILES += liquid/renderer.cpp \
+		liquid/pass_particles.cpp \
+		liquid/pass_metaballs.cpp \
+		liquid/pass_liquid.cpp
 endif
 
-LOCAL_C_INCLUDES += $(PROJECT_ROOT)
+LOCAL_C_INCLUDES += $(LOCAL_PATH)/..
 LOCAL_EXPORT_C_INCLUDES := $(LOCAL_C_INCLUDES)
 LOCAL_STATIC_LIBRARIES := elements_engine
 include $(BUILD_STATIC_LIBRARY)
 
 # elements engine
 # ------------------------------------
-ELEMENTS_PATH := $(PROJECT_ROOT)/elements
-include $(ELEMENTS_PATH)/Android.mk
+
+include $(LOCAL_PATH)/../elements/Android.mk
