94e53eefa256ed282b4dcf71c75d1264108bf2da
TheVice <thewinlab@hotmail.com>
2016-07-25 16:30:35 +0300

    [model_warehouse] as file already located inside elements folder it is not necessary put this folder name when includes other files from elements folder.

--- elements/rendering/models/model_warehouse.cpp
+++ elements/rendering/models/model_warehouse.cpp
@@ -2,7 +2,7 @@
 
 #include "assets/assets_storage.h"
 #include "assets/asset_texture.h"
-#include "elements/rendering/state/state_macro.h"
+#include "rendering/state/state_macro.h"
 #include "rendering/utils/program_loader.h"
 
 namespace eps {
